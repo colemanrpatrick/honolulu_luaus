721a641a542c12e2425dc4da22713f4731661c4b
patrick <patrickcoleman@Patricks-MacBook-Air-2.local>
2019-02-14 19:03:47 -0500

    first commit
    first commit

---
+++ gallery.js
@@ -0,0 +1,54 @@
+console.log("gallery.js ready");
+
+
+
+var slideIndex;
+
+var slides = document.getElementsByClassName("banner-section");
+
+var galBtn = document.getElementsByClassName("button-current");
+
+slideIndex = 1;
+
+gallerySlider(slideIndex);
+
+/*========*/
+function sliderCurrent(num){
+
+  gallerySlider(slideIndex = num);
+
+}
+function sliderPlus(num){
+
+  gallerySlider(slideIndex += num);
+
+}
+/*========*/
+function gallerySlider(num){
+
+  if(num > slides.length){
+    slideIndex = 1;
+  }
+
+  if(num < 1){
+    slideIndex = slides.length;
+  }
+
+  for (var i = 0; i < galBtn.length; i++) {
+
+    galBtn[i].classList = "button-current";
+
+  }
+
+  for (var i = 0; i < slides.length; i++) {
+
+    slides[i].classList = "banner-section";
+
+  }
+
+  slides[slideIndex-1].classList = "banner-section active";
+
+  galBtn[slideIndex-1].classList = "button-current active";
+
+}
+/*========*/
